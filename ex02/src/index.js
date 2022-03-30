81ee600399453a831043ad50c5d548e47a293383
ana-brdar <brdar.ana@yahoo.com>
2022-03-31 00:27:58 +0200

    Added two comments in index

---
+++ ex02/src/index.js
@@ -0,0 +1,12 @@
+function main() {
+    // Only change code below this line
+     /* This is a multi-line
+      comment! */
+      // Another single-line
+    // Only change code above this line
+
+}
+
+console.log(main());
+
+module.exports = main;
